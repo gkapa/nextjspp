a01ee7088540ed68094c62bbf019459960e8322a
HSH <pleasmin@gmail.com>
2021-02-10 19:05:31 +0900

    practice

---
+++ clone/nextjs-boilerplate/src/atoms/Hide.ts
@@ -0,0 +1,45 @@
+import styled, { css } from "styled-components";
+
+export const deviceSize = {
+  mobile: {
+    min: "1px",
+    max: "768px",
+  },
+  tablet: {
+    min: "769px",
+    max: "1024px",
+  },
+  laptop: {
+    min: "1025px",
+    max: "1440px",
+  },
+};
+
+export const media = {
+  lessThanMobile: `@media(max-width: ${deviceSize.mobile.min})`,
+  lessThanTablet: `@media(max-width: ${deviceSize.tablet.min})`,
+  lessThanLaptop: `@media(max-width: ${deviceSize.laptop.min})`,
+  greaterThanMobile: `@media(min-width: ${deviceSize.mobile.max})`,
+  greaterThanTablet: `@media(min-width: ${deviceSize.mobile.max})`,
+  greaterThanLaptop: `@media(min-width: ${deviceSize.mobile.max})`,
+};
+
+interface IProps {
+  when:
+    | "lessThanMobile"
+    | "lessThanTablet"
+    | "lessThanLaptop"
+    | "greaterThanMobile"
+    | "greaterThanTablet"
+    | "greaterThanLaptop";
+}
+
+export default styled.div.attrs(() => ({}))<IProps>`
+  ${(p) => {
+    return css`
+      ${media[p.when]} {
+        display: none;
+      }
+    `;
+  }}
+`;
---
+++ clone/nextjs-boilerplate/src/store/redux/navbar.ts
@@ -0,0 +1,36 @@
+const HIDE = "navbar/HI" as const;
+const UNHIDE = "navbar/LO" as const;
+
+export const hide = () => ({
+  type: HIDE,
+});
+export const unHide = () => ({
+  type: UNHIDE,
+});
+
+type Action = ReturnType<typeof hide | typeof unHide>;
+
+interface State {
+  isHide: boolean;
+}
+
+const initialState: State = {
+  isHide: true,
+};
+
+export default function fun(state = initialState, action: Action) {
+  switch (action.type) {
+    case HIDE:
+      return {
+        ...state,
+        isHide: true,
+      };
+    case UNHIDE:
+      return {
+        ...state,
+        isHide: false,
+      };
+    default:
+      return state;
+  }
+}
